76d15b398bf2bc9d8bc145b8eeb26058820041f6
Kelly Douglass <kdouglass@gmail.com>
2018-12-18 14:34:45 -0600

    Initial Commit

---
+++ SwipeGestures/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwipeGestures
+//
+//  Created by Kelly Douglass on 12/18/18.
+//  Copyright © 2018 Kelly Douglass. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
